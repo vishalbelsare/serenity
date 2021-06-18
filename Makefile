f8107475190b659d39dc696b78c8feee616a7938
Kyle Downey <kyle.downey@gmail.com>
2021-06-18 19:30:08 -0400

    improved build & deploy automation
    AB#110

--- Makefile
+++ Makefile
@@ -1,7 +1,14 @@
 # all our targets are phony (no files to check).
-.PHONY: help build rebuild prune publish
+.PHONY: help build build-base tag-testing tag-prod prune publish
 
+BASE_RELEASE=localhost:5000/cloudwallcapital/serenity-base:latest
+BASE_BUILD_NUMBER_FILE=base-build-number.txt
 BUILD_NUMBER_FILE=build-number.txt
+DATE_TAG=`date +%Y.%m.%d`
+BASE_BUILD_TAG=$(DATE_TAG)-b`cat $(BUILD_NUMBER_FILE)`
+BUILD_TAG=$(DATE_TAG)-b`cat $(BUILD_NUMBER_FILE)`
+BASE_IMAGE_NAME=cloudwallcapital/serenity-base:$(BASE_BUILD_TAG)
+IMAGE_NAME=cloudwallcapital/serenity:$(BUILD_TAG)
 
 help:
 	@echo ''
@@ -11,12 +18,45 @@ help:
 	@echo '  prune    	shortcut for docker system prune -af; cleans up inactive containers and cache'
 	@echo ''
 
+base-build:
+	@echo $$(($$(cat $(BASE_BUILD_NUMBER_FILE)) + 1)) > $(BASE_BUILD_NUMBER_FILE)
+	docker build --no-cache -t $(BASE_IMAGE_NAME) -f kubernetes/dockerfiles/serenity-base/Dockerfile .
+	docker image tag $(BASE_IMAGE_NAME) cloudwallcapital/serenity-base:latest
+
 build:
 	@echo $$(($$(cat $(BUILD_NUMBER_FILE)) + 1)) > $(BUILD_NUMBER_FILE)
-	docker build --no-cache -t cloudwallcapital/serenity:`date +%Y.%m.%d`-b`cat $(BUILD_NUMBER_FILE)` .
+	docker build --build-arg BASE_IMAGE_NAME=$(BASE_RELEASE) --no-cache -t $(IMAGE_NAME) -f kubernetes/dockerfiles/serenity/Dockerfile .
+	docker image tag $(IMAGE_NAME) cloudwallcapital/serenity:latest
 
-push:
-	docker push cloudwallcapital/serenity:`date +%Y.%m.%d`-b`cat $(BUILD_NUMBER_FILE)`
+base-push-latest:
+	docker push cloudwallcapital/serenity-base:latest
+
+base-push-testing:
+	docker image tag $(BASE_IMAGE_NAME) cloudwallcapital/serenity-base:testing
+	docker push cloudwallcapital/serenity-base:latest
+
+base-push-prod:
+	docker image tag $(BASE_IMAGE_NAME) cloudwallcapital/serenity-base:prod
+	docker push cloudwallcapital/serenity-base:latest
+
+base-push-local:
+	docker image tag $(BASE_IMAGE_NAME) localhost:5000/cloudwallcapital/serenity-base:latest
+	docker push localhost:5000/cloudwallcapital/serenity-base:latest
+
+push-latest:
+	docker push cloudwallcapital/serenity:latest
+
+push-testing:
+	docker image tag $(IMAGE_NAME) cloudwallcapital/serenity:testing
+	docker push cloudwallcapital/serenity:testing
+
+push-prod:
+	docker image tag $(IMAGE_NAME) cloudwallcapital/serenity:prod
+	docker push cloudwallcapital/serenity:prod
+
+push-local:
+	docker image tag $(IMAGE_NAME) localhost:5000/cloudwallcapital/serenity:latest
+	docker push localhost:5000/cloudwallcapital/serenity:latest
 
 prune:
 	# clean all that is not actively used
--- kubernetes/kubernetes_install.py
+++ kubernetes/kubernetes_install.py
@@ -1,15 +1,102 @@
+import glob
+import subprocess
+import tempfile
+
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 from os import path
 
+import fire
 
-def open_kubernetes_yaml(rel_path: str):
-    return open(path.join(path.dirname(__file__), f'{rel_path}'))
+import yaml
 
 
-def deploy(rel_path: str):
-    print(open_kubernetes_yaml(rel_path))
+class Deployer:
+    def __init__(self, env: str):
+        self.env = env
+        self.tmpl_env = Environment(
+            loader=FileSystemLoader(Deployer._local_path('')),
+            autoescape=select_autoescape()
+        )
+
+        config_file = open(Deployer._local_path(f'environments/{self.env}.yaml'), 'r')
+        self.config = yaml.safe_load(config_file)
+
+    @staticmethod
+    def helm_install(app: str, chart: str, overlay_values: str = None, extra_args: list = None):
+        args = ['helm ', 'install', app, chart]
+        if overlay_values is not None:
+            args.append(['-f', overlay_values])
+        if extra_args is not None:
+            args.extend(extra_args)
+
+    def kube_install(self, yaml_path: str, namespace: str = None):
+        if self.env == 'dev':
+            kubectl = 'kubectl'
+        else:
+            kubectl = 'microk8s.kubectl'
+
+        cmd = [kubectl, 'apply', '-f', yaml_path]
+        if namespace is not None:
+            cmd.extend(['-n', namespace])
+        Deployer._run_command(cmd)
+
+    def deploy_all(self, base_dir: str):
+        scan_dir = Deployer._local_path(f'{base_dir}')
+        input_paths = []
+        for ext in ('*.yaml', '*.yaml.jinja2'):
+            input_paths.extend(glob.glob(f'{scan_dir}/{ext}'))
+        for input_path in input_paths:
+            self.deploy(input_path)
+
+    def deploy(self, input_path: str):
+        print(f'processing {input_path}')
+        if input_path.endswith('.jinja2'):
+            # a bit ugly: reverse out to get back the relative path
+            # for Jinja2 template resolution
+            rel_path = path.relpath(input_path, path.dirname(__file__))
+            yaml_tmpl = self.tmpl_env.get_template(rel_path)
+            generated_yaml_txt = yaml_tmpl.render(self.config)
+            with tempfile.NamedTemporaryFile() as tmp:
+                tmp.write(generated_yaml_txt.encode('utf-8'))
+                tmp.flush()
+                self.kube_install(tmp.name)
+        else:
+            self.kube_install(input_path)
+
+    @staticmethod
+    def _local_path(rel_path: str):
+        return path.join(path.dirname(__file__), f'{rel_path}')
+
+    @staticmethod
+    def _run_command(args: list):
+        subprocess.run(args)
+
+
+# noinspection PyDefaultArgument
+def install_serenity(env: str = 'dev', components: list = ['core', 'db', 'infra', 'strategies', 'research']):
+    component_set = set([x.lower() for x in components])
+    deployer = Deployer(env)
+
+    # install Helm charts for infrastructure
+    deployer.helm_install('consul', 'hashicorp/consol', f'kubernetes/consul/{env}-config.yaml',
+                          ['--set', 'global.name=consul'])
+
+    # install requested Kubernetes objects
+    if 'core' in component_set:
+        deployer.deploy_all('feedhandlers')
+
+    if 'db' in component_set:
+        deployer.deploy_all('db')
+
+    if 'infra' in component_set:
+        deployer.deploy_all('logging')
+
+    if 'strategies' in component_set:
+        deployer.deploy_all('strategies')
+
+    if 'research' in component_set:
+        deployer.deploy_all('research')
 
 
 if __name__ == '__main__':
-    # helm install consul hashicorp/consul --set global.name=consul -f kubernetes/consul/dev-config.yaml
-
-    deploy('research/01_serenity-jupyterlab-storage.yaml')
+    fire.Fire(install_serenity)
