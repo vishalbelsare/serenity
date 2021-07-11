9543c1ddf30effab525521c8ac0afee496e90a50
Kyle F. Downey <kyle.downey@gmail.com>
2021-07-11 15:44:25 -0400

    improved script for Minikube Docker registry to work out of the box
    AB#108

--- scripts/dev_mac_docker_registry.sh
+++ scripts/dev_mac_docker_registry.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-kubectl port-forward --namespace kube-system registry 5000:5000 &
+minikube addons enable registry
+
+REGISTRY_POD=$(kubectl get pod -l kubernetes.io/minikube-addons=registry -n kube-system -o jsonpath="{.items[0].metadata.name}")
+kubectl port-forward --namespace kube-system "$REGISTRY_POD" 5000:5000 &
 docker run --rm -it --network=host alpine ash -c "apk add socat && socat TCP-LISTEN:5000,reuseaddr,fork TCP:host.docker.internal:5000"
