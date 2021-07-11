e2f6641af9139aa743271062bbb190e5feac5363
Kyle F. Downey <kyle.downey@gmail.com>
2021-07-11 15:31:57 -0400

    drop installer for VictoriaMetrics helm repo
    AB#108

--- scripts/dev_mac_install.sh
+++ scripts/dev_mac_install.sh
@@ -102,7 +102,6 @@ fi
 
 # set up Helm repositories
 helm repo add hashicorp https://helm.releases.hashicorp.com
-helm repo add vm https://victoriametrics.github.io/helm-charts/
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
